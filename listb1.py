91b6b5f428875613954014510c85f8ae2e6f3501
Nater0214 <gamer.nater0214@gmail.com>
2022-10-16 17:50:15 -0400

    Inherit from list instead of defining things

--- listb1.py
+++ listb1.py
@@ -7,78 +7,43 @@ from typing import Any
 
 
 # Definitions
-class Listb1:
+class Listb1(list):
     """Creates a 1 based list, instead of the usual 0 based one"""
-
-    # Variables
-    l = []
-    
-
-    # Definitions
-    def __init__(self, l: Iterable) -> None:
-        self.l = list(l)
-    
-    def __iter__(self) -> list:
-        return self.l
     
     def __getitem__(self, i: int) -> Any:
         if i == 0:
             raise IndexError("list index out of range")
         elif i > 0:
-            return self.l[i-1]
+            return super().__getitem__(i-1)
         else:
-            return self.l[i]
+            return super().__getitem__(i)
     
-    def __setitem__(self, i: int, v: Any) -> None:
+    def __setitem__(self, i: int, val: Any) -> None:
         if i == 0:
             raise IndexError("list index out of range")
         elif i > 0:
-            self.l[i-1] = v
+            super().__setitem__(i-1)
         else:
-            self.l[i] = v
-    
-    def __str__(self) -> str:
-        return f"Listb1({self.l})"
-    
-    def append(self, v: Any) -> None:
-        self.l.append(v)
-    
-    def clear(self) -> None:
-        self.l = []
-    
+            self.l[i] = val
+       
     def copy(self) -> Any:
-        return Listb1(self.l)
+        return Listb1(super().copy()())
     
-    def count(self, v: Any) -> int:
-        return self.l.count(v)
+    def index(self, val: Any) -> int:
+        return super().index(val) + 1
 
-    def extend(self, l: Iterable) -> None:
-        self.l += list(l)
-    
-    def index(self, v: Any) -> int:
-        return self.l.index(v) + 1
-
-    def insert(self, i: int, v: Any) -> None:
+    def insert(self, i: int, val: Any) -> None:
         if i == 0:
             raise IndexError("list index out of range")
         elif i > 0:
-            self.l.insert(i-1, v)
+            super().insert(i-1, val)
         else:
-            self.l.insert(i, v)
+            super().insert(i, val)
     
     def pop(self, i: int) -> None:
         if i == 0:
             raise IndexError("list index out of range")
         elif i > 0:
-            self.l.pop(i-1)
+            super().pop(i-1)
         else:
-            self.l.pop(i)
-    
-    def remove(self, v: Any) -> None:
-        self.l.remove(v)
-    
-    def reverse(self) -> None:
-        self.l.reverse()
-    
-    def sort(self) -> None:
-        self.l.sort()
+            super().pop(i)
